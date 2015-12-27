45b6587c20d0f9727cf56443d9001c5877e183be
Galib Ahmed Choudhury <Galib Ahmed Choudhury>
2015-12-27 15:34:23 -0600

    adding Project

---
+++ Project1/src/Bnz.java
@@ -0,0 +1,17 @@
+
+public class Bnz {
+	int e =5;
+	int z =8;
+	
+	void b()
+	{
+		System.out.println(e+z);
+		System.out.println(e-z);
+	}
+	
+	void n()
+	{
+		System.out.println(e*z);
+		System.out.println(10-5);
+	}
+}
---
+++ Project1/src/Nsn.java
@@ -0,0 +1,9 @@
+
+public class Nsn {
+	public static void main(String[] atgs){
+	Bnz y = new Bnz();
+	y.b();
+	y.n();
+	
+	}
+}
